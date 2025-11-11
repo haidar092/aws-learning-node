96fd9a2ce8344cc0c3dea93f45204530317a8830
sameeworking2000 <89939942+sameeworking2000@users.noreply.github.com>
2025-11-11 12:43:04 +0500

    env updated to atlas

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
+  console.log(process.env.MONGO_URI);
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/schoolDB");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ Mongo Error:", error);
